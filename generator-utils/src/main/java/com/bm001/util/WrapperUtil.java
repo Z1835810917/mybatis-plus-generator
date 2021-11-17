5bd41140391cf23959be34716b0334addfd864da
chenmingjun <mingjun.chen@sotos.com.cn>
2021-11-17 13:49:29 +0800

    更新WrapperUtil

--- generator-utils/src/main/java/com/bm001/util/WrapperUtil.java
+++ generator-utils/src/main/java/com/bm001/util/WrapperUtil.java
@@ -5,7 +5,10 @@ import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.bm001.annotation.QueryField;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -92,4 +95,36 @@ public class WrapperUtil {
         return sb.toString();
     }
 
+	public static void main(String[] args) {
+
+
+			Random random=new Random();
+
+			List<Integer> list=new ArrayList<>();
+			for(int i=0;i<10000000;i++)
+			{
+				int randomResult=random.nextInt(100000000);
+				list.add(randomResult);
+			}
+			System.out.println("产生的随机数有");
+			for(int i=0;i<list.size();i++)
+			{
+				System.out.println(list.get(i));
+			}
+			BitSet bitSet=new BitSet(100000000);
+			for(int i=0;i<10000000;i++)
+			{
+				bitSet.set(list.get(i));
+			}
+
+			System.out.println("0~1亿不在上述随机数中有"+bitSet.cardinality());
+			for (int i = 0; i < 100000000; i++)
+			{
+				if(!bitSet.get(i))
+				{
+					System.out.println(i);
+				}
+			}
+
+	}
 }
