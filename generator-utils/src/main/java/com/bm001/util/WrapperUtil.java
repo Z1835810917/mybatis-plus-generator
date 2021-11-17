b46b8cf6895eba0fe204e5dec8890c7f3d51124e
chenmingjun <mingjun.chen@sotos.com.cn>
2021-11-17 15:20:33 +0800

    更新WrapperUtil

--- generator-utils/src/main/java/com/bm001/util/WrapperUtil.java
+++ generator-utils/src/main/java/com/bm001/util/WrapperUtil.java
@@ -6,6 +6,7 @@ import com.bm001.annotation.QueryField;
 
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -33,11 +34,15 @@ public class WrapperUtil {
 			QueryField annotation = field.getAnnotation(QueryField.class);
 			String colName = annotation.value();
 			//转化条件 1、Entity必须有值 2、必须有QueryField注解
-			if (obj != null && annotation != null) {
+			if (Objects.nonNull(obj) &&  Objects.nonNull(annotation)  ) {
+				//如果注解的value没有值，就把变量名赋值给colName，再转驼峰成为类名
+				if(StringUtils.isBlank(annotation.value())){
+					colName=fieldName;
+				}
 				//注解转化为查询条件
 				switch (annotation.type()) {
 					case EQ:
-						queryWrapper.eq(StringUtils.isNotBlank((CharSequence) obj), StringUtils.isBlank(colName) ? fieldName : humpToLine(colName), obj);
+						queryWrapper.eq(StringUtils.isNotBlank((CharSequence) obj),  humpToLine(colName), obj);
 						break;
 					case LIKE:
 						queryWrapper.like(StringUtils.isNotBlank((CharSequence) obj), humpToLine(colName), obj);
